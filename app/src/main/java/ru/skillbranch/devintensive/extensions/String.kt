088387e50a7cc8100006e97e355e51771517c5d5
4050 <stasguest@gmail.com>
2019-07-10 16:19:11 +0300

    Update String.kt

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,16 +1,35 @@
 package ru.skillbranch.devintensive.extensions
 
-fun String.truncate(len:Int=16):String{
-    val newValue = this.trimEnd()
-    return newValue.take(len+1).trimEnd() + if (newValue.length > len) "..." else ""
+fun String.truncate(length: Int = 16): String {
+    return if (this.trimEnd().length <= length) this.trimEnd() else "${this.substring(0, length).trimEnd()}..."
 }
 
-fun String.stripHtml():String{
-    return this
-        .replace(Regex("(=(.*?)(\"(.*?)(?<!\\\\)\"|'(.*?)(?<!\\\\)').*?)"),"")
-        .replace("&nbsp;"," ")
-        .replace(Regex("&(.*?);"),"")
-        .replace(Regex("<[^>]*>"),"")
-        .replace("\n","")
-        .replace(Regex(" {2,}")," ")
+fun String.stripHtml(): String {
+    return this.stripHtmlTags().stripHtmlEscapeSequences().stripMultipleWhitespaces()
+}
+
+fun String.stripHtmlTags(): String {
+    var result: String = this
+    var processedLength = 0
+    do {
+        val tagOpenPosition = result.indexOf("<", processedLength)
+        val tagClosePosition = if (tagOpenPosition >= 0) result.indexOf(">", tagOpenPosition) else -1
+        if (tagOpenPosition >= 0 && tagClosePosition >= 0)
+        {
+            result = result.removeRange(tagOpenPosition, tagClosePosition + 1)
+            processedLength = tagOpenPosition
+        }
+    } while (tagOpenPosition >= 0 && tagClosePosition >= 0)
+
+    return result
+}
+
+fun String.stripHtmlEscapeSequences(): String {
+    val regex = Regex("""&amp(;)?|&lt(;)?|&gt(;)?|&quot(;)?|&apos(;)?|&#\d{1,}(;)?""")
+    return regex.replace(this, "")
+}
+
+fun String.stripMultipleWhitespaces(): String {
+    val regex = Regex(""" {1,}""")
+    return regex.replace(this, " ")
 }
