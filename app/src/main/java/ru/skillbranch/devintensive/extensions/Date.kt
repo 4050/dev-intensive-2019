9ea2d683dbf7ece2367fefde7253257cd70faac9
4050 <stasguest@gmail.com>
2019-09-07 23:01:37 +0300

    hometask_5

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -0,0 +1,117 @@
+package ru.skillbranch.devintensive.extensions
+
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.abs
+import kotlin.math.roundToInt
+
+const val SECOND = 1000L
+const val MINUTE = 60 * SECOND
+const val HOUR = 60 * MINUTE
+const val DAY = 24 * HOUR
+
+
+fun Date.format(pattern:String="HH:mm:ss dd.MM.yy"):String{
+    val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
+
+    return dateFormat.format(this)
+}
+
+fun Date.shortFormat(): String {
+    val pattern = if (this.isSameDay(Date())) "HH:mm" else "dd.MM.yy"
+    return this.format(pattern)
+}
+
+fun Date.isSameDay(date: Date): Boolean {
+    val day1 = this.time / DAY
+    val day2 = date.time / DAY
+
+    return day1 == day2
+}
+
+fun Date.add(value:Int, units:TimeUnits=TimeUnits.SECOND):Date{
+    var time = this.time
+
+    time += when (units){
+        TimeUnits.SECOND -> value * SECOND
+        TimeUnits.MINUTE -> value * MINUTE
+        TimeUnits.HOUR -> value * HOUR
+        TimeUnits.DAY -> value * DAY
+    }
+    this.time = time
+    return this
+}
+
+fun Date.humanizeDiff(date: Date=Date()): String {
+    fun getTimeUnitValue(diff:Long, unit:TimeUnits):String{
+        val unitDiff = diff.toDouble() /
+                when (unit) {
+                    TimeUnits.SECOND -> SECOND
+                    TimeUnits.MINUTE -> MINUTE
+                    TimeUnits.HOUR -> HOUR
+                    TimeUnits.DAY -> DAY
+                }
+
+        return unit.plural(unitDiff.roundToInt())
+
+    }
+
+    val timeDiff = (date.time - this.time)
+    val timeDiffUnsigned = abs(timeDiff)
+    val postfix = if (timeDiff>0) " назад" else ""
+    val prefix = if (timeDiff<0) "через " else ""
+
+    return when(timeDiffUnsigned){
+        in 0 .. SECOND            -> "только что"
+        in SECOND .. 45*SECOND    -> "${prefix}несколько секунд$postfix"
+        in 45*SECOND .. 75*SECOND -> "${prefix}минуту$postfix"
+        in 75*SECOND .. 45*MINUTE -> "$prefix${getTimeUnitValue(timeDiffUnsigned, TimeUnits.MINUTE)}$postfix"
+        in 45*MINUTE .. 75*MINUTE -> "${prefix}час$postfix"
+        in 75*MINUTE .. 22*HOUR   -> "$prefix${getTimeUnitValue(timeDiffUnsigned, TimeUnits.HOUR)}$postfix"
+        in 22*HOUR .. 26*HOUR     -> "${prefix}день$postfix"
+        in 26*HOUR .. 360*DAY     -> "$prefix${getTimeUnitValue(timeDiffUnsigned, TimeUnits.DAY)}$postfix"
+        else                         -> if (timeDiff>0) "более года назад" else "более чем через год"
+    }
+}
+
+
+enum class TimeUnits{
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY;
+
+    fun plural(value:Int):String{
+        val unitValue = value.toString()
+        return "$unitValue "+when (this){
+            SECOND ->{"секунд"+
+                    when (unitValue[unitValue.lastIndex]){
+                        '1' -> if(unitValue.getOrNull(unitValue.lastIndex-1)=='1') "" else "у"
+                        in '2'..'4' -> if(unitValue.getOrNull(unitValue.lastIndex-1)=='1') "" else "ы"
+                        else -> ""
+                    }
+            }
+            MINUTE -> {"минут"+
+                    when (unitValue[unitValue.lastIndex]){
+                        '1' -> if(unitValue.getOrNull(unitValue.lastIndex-1)=='1') "" else "у"
+                        in '2'..'4' -> if(unitValue.getOrNull(unitValue.lastIndex-1)=='1') "" else "ы"
+                        else -> ""
+                    }
+            }
+            HOUR -> {"час"+
+                    when (unitValue[unitValue.lastIndex]){
+                        '1' -> if(unitValue.getOrNull(unitValue.lastIndex-1)=='1') "ов" else ""
+                        in '2'..'4' -> if(unitValue.getOrNull(unitValue.lastIndex-1)=='1') "ов" else "а"
+                        else -> "ов"
+                    }
+            }
+            DAY -> {"д"+
+                    when (unitValue[unitValue.lastIndex]){
+                        '1' -> if(unitValue.getOrNull(unitValue.lastIndex-1)=='1') "ней" else "ень"
+                        in '2'..'4' -> if(unitValue.getOrNull(unitValue.lastIndex-1)=='1') "ней" else "ня"
+                        else -> "ней"
+                    }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,158 @@
+package ru.skillbranch.devintensive.utils
+
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
+import java.util.*
+import android.graphics.Paint.ANTI_ALIAS_FLAG
+import android.text.TextPaint
+import android.content.Context
+import android.graphics.*
+import android.util.Log
+import android.util.TypedValue
+import androidx.core.graphics.drawable.toDrawable
+import ru.skillbranch.devintensive.R
+import kotlin.math.round
+
+
+object Utils {
+    fun parseFullName(fullName:String?) : Pair<String?, String?> {
+         val mFullName = fullName?.trim()?.replace(Regex(" {2,}")," ")
+        val parts : List<String>? = if (mFullName.isNullOrBlank()) null else mFullName.split(" ")
+
+        val firstName = parts?.getOrNull(0)
+        val lastName = parts?.getOrNull(1)
+
+        //return Pair(firstName, lastName)
+        return (if (firstName.isNullOrBlank()) null else firstName) to (if (lastName.isNullOrBlank()) null else lastName)
+    }
+
+    fun transliteration(payload: String, divider: String=" ") : String {
+        fun transliterateChar(char:String):String {
+            return when (char) {
+                "а" -> "a"
+                "б" -> "b"
+                "в" -> "v"
+                "г" -> "g"
+                "д" -> "d"
+                "е" -> "e"
+                "ё" -> "e"
+                "ж" -> "zh"
+                "з" -> "z"
+                "и" -> "i"
+                "й" -> "i"
+                "к" -> "k"
+                "л" -> "l"
+                "м" -> "m"
+                "н" -> "n"
+                "о" -> "o"
+                "п" -> "p"
+                "р" -> "r"
+                "с" -> "s"
+                "т" -> "t"
+                "у" -> "u"
+                "ф" -> "f"
+                "х" -> "h"
+                "ц" -> "c"
+                "ч" -> "ch"
+                "ш" -> "sh"
+                "щ" -> "sh'"
+                "ъ" -> ""
+                "ы" -> "i"
+                "ь" -> ""
+                "э" -> "e"
+                "ю" -> "yu"
+                "я" -> "ya"
+                " " -> divider
+                else -> char
+            }
+        }
+
+        var result = ""
+        for (char in payload){
+            result += if(char.isUpperCase()) transliterateChar(char.toLowerCase().toString()).capitalize()
+                      else transliterateChar(char.toString())
+        }
+
+        return result
+    }
+
+    fun toInitials(firstName: String?, lastName: String?) : String? {
+        fun getInitial(word:String?) = word?.trim()?.toUpperCase(Locale("ru"))?.getOrNull(0)
+
+        val firstInitial = getInitial(firstName)
+        val lastInitial = getInitial(lastName)
+        return if (firstInitial==null && lastInitial==null) null else "${firstInitial ?: ""}${lastInitial ?: ""}"
+    }
+
+    fun validateRepository(repo: String) : Boolean{
+        val repoPattern = Regex("^(?:https://|https:\\\\\\\\|)(?:www\\.|)github\\.com/((?:(?:\\d|\\w)|(?:\\d|\\w)-)*(?:\\d|\\w)|(?:\\d|\\w))(?:/|\\\\|)\$", setOf(RegexOption.IGNORE_CASE))
+        val githubPattern = Regex("^(?:https://|https:\\\\\\\\|)(?:www\\.|)github\\.com/",setOf(RegexOption.IGNORE_CASE))
+        val restrictedWords = arrayOf(
+                "enterprise",
+                "features",
+                "topics",
+                "collections",
+                "trending",
+                "events",
+                "marketplace",
+                "pricing",
+                "nonprofit",
+                "customer-stories",
+                "security",
+                "login",
+                "join"
+        )
+        return if (repoPattern.matches(repo)){
+            val userName = repo
+                    .replace(githubPattern, "")
+                    .replace("/","")
+                    .replace("\\","")
+            restrictedWords.indexOf(userName) == -1
+        }else{
+            false
+        }
+    }
+
+    fun getInitialsBitmap(cont: Context, initials:String): Bitmap? {
+        if (initials.isEmpty()) return null
+
+        val textSizeCoef = 0.43f
+        val imageSize = cont.resources.getDimensionPixelSize(R.dimen.avatar_round_size)
+        val tv = TypedValue()
+        val a = cont.obtainStyledAttributes(tv.data, intArrayOf(R.attr.colorAccent))
+
+
+        val rectF = RectF()
+        with(rectF) {
+            bottom = imageSize.toFloat()
+            right = imageSize.toFloat()
+        }
+        val paint = Paint(ANTI_ALIAS_FLAG)
+        paint.color = a.getColor(0,0)
+
+        val textWidth: Float
+        val textBaseLine: Float
+        val textPaint = TextPaint(ANTI_ALIAS_FLAG)
+
+        with(textPaint){
+            textSize = imageSize * textSizeCoef //* cont.resources.displayMetrics.scaledDensity
+            color = Color.WHITE
+            textWidth = measureText(initials) * 0.5f
+            textBaseLine = fontMetrics.ascent * -0.4f
+        }
+
+        val centerX = round(imageSize * 0.5f)
+        val centerY = round(imageSize * 0.5f)
+
+        val bitmap = Bitmap.createBitmap(imageSize, imageSize, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+
+        with (canvas) {
+            drawRect(rectF, paint)
+            drawText(initials, centerX - textWidth, centerY + textBaseLine, textPaint)
+        }
+
+        a.recycle()
+        return bitmap
+    }
+}
